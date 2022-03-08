bdca75de8db5367f22b98184cec7b16c9edea89d
bijoy32145 <95517681+bijoy32145@users.noreply.github.com>
2022-03-08 23:29:35 +0530

    Add files via upload

---
+++ exam  1.py
@@ -0,0 +1,107 @@
+####q:write a programme to find reverse of a string using while loop ?
+# str = input('enter a name')
+# i = len(str)-1
+# while(i>=0):
+#     print(str[i],end='')
+#     i-=1
+###q:check wheather the given string is palandrome or not
+# str = input('enter the string')
+# d = len(str)-1
+# temp = str
+# i = len(temp)-1
+# while(i>0):
+#     i-=1
+# if temp[i] == str[d]:
+#     print('  palandrome')
+# else:
+#     print('  not a pellondrome')
+
+####area of a triangle
+# b = int(input('enter the base '))
+# h = int(input('enter the height'))
+# a = b*h/2
+# print('area=',a)
+
+
+###find the iteration variable in the following python code
+#for letter in 'apple'
+  # print("letter")
+  #ans : letter
+
+
+##write the following python code printout?
+#print(len('luminar')*7)
+#ans = 49
+
+
+####how would you print out the following variable into upper case in python?
+#Greet = Hello World
+# a = " Greet =Hello World".upper()
+# print(a)
+
+###what is the out of the following code?
+# a = "hello"
+# b = "welcome"
+# print(a+b)
+###find the program print out
+# x = '40'
+# y = int(x)+2
+# print(y)
+###print the series using for loop
+# #100,90,80,....,10
+# for i in range(100,0,-10):
+#     print(i,",",end='')
+###qestion num 11
+# i = int(input('enter the unit from user'))
+# if i<=100:
+#     print("no charge")
+# elif 100<i<=200:
+#     a= (i-100*5)
+#     print(a)
+# else:
+#     b =((i-200)*10)+500
+#     print(b)
+# ### write a pgm to check whether the last digit of a number(enter by user0) is divisible by 3 or not
+# num = int(input('enter the num'))
+# b=num%10
+# if b%3==0:#reminder
+#     print("it is divisible")
+# else :
+#     print("not divisible")
+###write the output of the following if a = 9
+# if (a>5 and a<=10):
+#     print("hello")
+# else:
+#     print('bye')
+#     ans : hello
+
+#qn14 accept the age ,sex , number of days,display the wages accordinvgly
+# age = int(input('enter the age'))
+# sex = input("sex")
+# nd = int(input("enter num of days"))
+# if (18<=age<30):
+#     if sex == 'M':
+#         print("wage = ",nd*700)
+#     elif sex == 'F':
+#         print("wage = ",nd*750)
+# elif (30<=age<=40):
+#     if sex == 'M':
+#         print("wage = ",nd*800)
+#     elif sex == 'F':
+#         print("wage = ",nd*850)
+
+##write the pgm to find factorial of a number using while loop
+# num = int(input('enter num'))
+# f = 1
+# while(num > 0):
+#     f = f *num
+#     num-=1
+# print("factorial = ",f)
+####write a pgm to print given output
+# a = 'hello'
+# l = int(len(a))
+# for i in range(l+1):#1,2,3,4,5
+#     for j in range(i):#h e l l o
+#       print(a[j],end='')
+#     print()
+
